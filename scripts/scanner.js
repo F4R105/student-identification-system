12841d79cf09c27c97fa9fce2defa95f582dddda
F4R105 <kajubufaraji@gmail.com>
2023-07-19 12:41:22 +0300

    add styles that handle approved text, add multiscan

--- scripts/scanner.js
+++ scripts/scanner.js
@@ -9,8 +9,14 @@ const showStudentInfo = (student) => {
 
     const studentInfoHTML = `
     <div>
-        <div id="studentImageContainer">
-            <img src="../../store/students_photos/${student.profile_picture}" alt="">
+        <div id="resultHeader">
+            <div id="studentImageContainer">
+                <img src="../../store/students_photos/${student.profile_picture}" alt="">
+            </div>
+            <div>
+                <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 640 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M96 128a128 128 0 1 1 256 0A128 128 0 1 1 96 128zM0 482.3C0 383.8 79.8 304 178.3 304h91.4C368.2 304 448 383.8 448 482.3c0 16.4-13.3 29.7-29.7 29.7H29.7C13.3 512 0 498.7 0 482.3zM625 177L497 305c-9.4 9.4-24.6 9.4-33.9 0l-64-64c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l47 47L591 143c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9z"/></svg>
+                <h1>Approved</h1>
+            </div>
         </div>
         <table>
             <tr>
@@ -37,6 +43,15 @@ const showStudentInfo = (student) => {
     template.innerHTML = studentInfoHTML.trim()
     scannerResults.appendChild(template.content.firstElementChild)
     scannerResults.style.display = 'flex'
+
+    setTimeout(()=>{
+        scannerResults.style.display = 'none'
+        scannerResults.innerHTML = ''
+        scannerInput.focus()
+        scannerInput.value = ''
+        scannerProgress.style.display = 'block'
+        scannerProgress.innerText = `Waiting for scan!..`
+    },2000)
 }
 
 const fetchStudentInfo = async (admissionNumber) => {
@@ -84,7 +99,9 @@ scanBtn.addEventListener('click',()=>{
 
     scannerProgress.innerText = `Waiting for scan!..`
 
-    setTimeout(()=>{fetchStudentInfo(20051013030)},3000)
+    // setTimeout(()=>{
+    //     fetchStudentInfo('20051013032')
+    // }, 2000)
 })
 
 scannerOverlay.addEventListener('click',(e)=>{
